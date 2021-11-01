e2b98054c6788a41e6a8889e332985105275edde
Fikcup <rhys.n.wright@gmail.com>
2021-11-01 11:24:24 -0700

    feat : Product model

--- server/models/Product.js
+++ server/models/Product.js
@@ -5,7 +5,23 @@ class Product extends Model {}
 
 Product.init(
     {
-
+        uuid: {
+            type: DataTypes.UUID,
+            defaultValue: DataTypes.UUIDV1,
+            primaryKey: true,
+            allowNull: false,
+            unique: true,
+        },
+        name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true,
+        },
+        description: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true,
+        }
     },
     {
         sequelize,
