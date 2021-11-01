e2456a41a49db2324d83c0d864d108cbcf4872cb
Fikcup <rhys.n.wright@gmail.com>
2021-11-01 11:23:38 -0700

    feat : Category model

--- server/models/Category.js
+++ server/models/Category.js
@@ -5,7 +5,18 @@ class Category extends Model {}
 
 Category.init(
     {
-
+        uuid: {
+            type: DataTypes.UUID,
+            defaultValue: DataTypes.UUIDV1,
+            primaryKey: true,
+            allowNull: false,
+            unique: true,
+        },
+        name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true,
+        }
     },
     {
         sequelize,
