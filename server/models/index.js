2e41404e94a8f2ff9f9ad91fcf5c376d81ea1bf8
Fikcup <rhys.n.wright@gmail.com>
2021-11-03 22:07:22 -0700

    feat : starting model association code

--- server/models/index.js
+++ server/models/index.js
@@ -2,5 +2,11 @@ const User = require('./User');
 const Product = require('./Product');
 const Category = require('./Category');
 const Cart = require('./Cart');
+const CartProducts = require('./CartProducts');
 
-module.exports = { User, Product, Category, Cart };
+// TODO: Fix model associations
+User.hasOne(Cart, {foreignKey: { allowNull: true }, onDelete: 'CASCADE'});
+Category.hasMany(Product, {foreignKey: { allowNull: false }});
+Product.belongsToMany(Cart, { through: 'CartProducts' }, {foreignKey: { allowNull: true }, onDelete: 'CASCADE'});
+
+module.exports = { User, Product, Category, Cart, CartProducts };
