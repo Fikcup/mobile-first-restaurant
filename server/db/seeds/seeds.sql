c01efce460e353c37ef47c93e156c70d28dc2278
Fikcup <rhys.n.wright@gmail.com>
2021-11-08 13:57:18 -0800

    feat : initial seed files

---
+++ server/db/seeds/seeds.sql
@@ -0,0 +1,11 @@
+USE buffet_db;
+
+-- convert to sequelize seed --
+INSERT INTO 
+    category(name)
+VALUES
+    ("Pancakes"),
+    ("Omelettes"),
+    ("Skillets"),
+    ("Sandwhiches"),
+    ("Waffles");
