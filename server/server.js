658f3439d927111beb88781c370d05e219423e94
Fikcup <rhys.n.wright@gmail.com>
2021-11-08 10:29:05 -0800

    feat : implemented routes to server

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const sequelize = require('./config/connection');
 const models = require('./models');
+const routes = require('./routes');
 require('dotenv').config();
 
 const app = express();
@@ -10,6 +11,8 @@ app.use(express.json());
 app.use(express.urlencoded({
     extended: true
 }));
+
+app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
     app.listen(PORT, () => {
