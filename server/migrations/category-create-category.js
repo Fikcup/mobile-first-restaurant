4efff870e888b583632fabfed0c045fe6b09eeb5
Fikcup <rhys.n.wright@gmail.com>
2021-11-08 18:29:42 -0800

    refactor : hard coded uuid for product seeds to reference

--- server/migrations/category-create-category.js
+++ server/migrations/category-create-category.js
@@ -1,14 +1,28 @@
 'use strict';
-const { v1: UUIDV1 } = require('uuid');
 
 module.exports = {
     up: async function (queryInterface, Sequelize) {
         await queryInterface.bulkInsert('Category', [
-            { uuid: UUIDV1(), name: 'pancakes' },
-            { uuid: UUIDV1(), name: 'omelettes' },
-            { uuid: UUIDV1(), name: 'skillets' },
-            { uuid: UUIDV1(), name: 'sandwhiches' },
-            { uuid: UUIDV1(), name: 'waffles' }
+            { 
+                uuid: '5bcead9a-4104-11ec-973a-0242ac130003', 
+                name: 'pancakes' 
+            },
+            { 
+                uuid: '683f1574-4104-11ec-973a-0242ac130003', 
+                name: 'omelettes' 
+            },
+            { 
+                uuid: '76a2d90c-4104-11ec-973a-0242ac130003',
+                name: 'skillets' 
+            },
+            { 
+                uuid: '8fef7b86-4104-11ec-973a-0242ac130003',
+                name: 'sandwhiches' 
+            },
+            { 
+                uuid: '939ec372-4104-11ec-973a-0242ac130003',
+                name: 'waffles' 
+            }
         ]);
     },
 
