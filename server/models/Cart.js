a6cf9c1e482c62a3e538beebe88742d870c21415
Fikcup <rhys.n.wright@gmail.com>
2021-11-08 18:19:33 -0800

    refactor : removed timestamps

--- server/models/Cart.js
+++ server/models/Cart.js
@@ -17,6 +17,7 @@ Cart.init(
         sequelize,
         freezeTableName: true,
         underscored: true,
+        timestamps: false,
         modelName: 'cart'
     }
 );
--- server/models/Category.js
+++ server/models/Category.js
@@ -22,6 +22,7 @@ Category.init(
         sequelize,
         freezeTableName: true,
         underscored: true,
+        timestamps: false,
         modelName: 'category'
     }
 );
