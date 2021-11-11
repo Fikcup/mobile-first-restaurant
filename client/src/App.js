6122a4810906e07ac7df33598a9f05867fdcb703
Fikcup <rhys.n.wright@gmail.com>
2021-11-11 13:14:39 -0800

    feat : added routes to conditionally render content

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { HashRouter as Router, Switch, Route } from 'react-router-dom'; 
+import { HashRouter as Router, Routes, Route } from 'react-router-dom'; 
 import Footer from './components/Footer';
 import Nav from './components/Nav';
 
@@ -7,6 +7,12 @@ function App() {
   return (
     <Router>
     <div className="App">
+      <Routes>
+        <Route exact path="/" />
+        <Route exact path="/menu" />
+        <Route exact path="/me" />
+        <Route exact path="/cart" />
+      </Routes>
       <Footer>
         <Nav />
       </Footer>
