609f6f33eb916966c1cb8ccadd450dd2c055ced8
Fikcup <rhys.n.wright@gmail.com>
2021-11-11 13:08:35 -0800

    fix : reset react app as application would not launch

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import { HashRouter as Router, Switch, Route } from 'react-router-dom'; 
+import Footer from './components/Footer';
+import Nav from './components/Nav';
 
 function App() {
   return (
+    <Router>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Footer>
+        <Nav />
+      </Footer>
     </div>
+    </Router>
   );
 }
 
