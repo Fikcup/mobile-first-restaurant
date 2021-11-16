696d32f546ac4bb47a0195e232a1c8c6a2e18232
Fikcup <rhys.n.wright@gmail.com>
2021-11-16 10:14:45 -0800

    fix : corrected CartProducts table associations

--- server/models/index.js
+++ server/models/index.js
@@ -6,6 +6,6 @@ const CartProducts = require('./CartProducts');
 
 User.hasOne(Cart, {foreignKey: { allowNull: true }, onDelete: 'CASCADE'});
 Category.hasMany(Product, {foreignKey: { allowNull: false }});
-Product.belongsToMany(Cart, { through: 'CartProducts' }, {foreignKey: { allowNull: true }, onDelete: 'CASCADE'});
+Product.belongsToMany(Cart, { through: CartProducts }, {foreignKey: { allowNull: true }, onDelete: 'CASCADE'});
 
 module.exports = { User, Product, Category, Cart, CartProducts };
