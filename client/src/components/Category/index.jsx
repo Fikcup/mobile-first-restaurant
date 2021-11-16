74da1fbf1578db66a9a44678f361fcadc77974e2
Fikcup <rhys.n.wright@gmail.com>
2021-11-16 13:34:45 -0800

    feat : category component

---
+++ client/src/components/Category/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Category = (props) => {
+    <div>
+        <h1>{props.name}</h1>
+    </div>
+}
+
+export default Category;
--- server/routes/menu-routes.js
+++ server/routes/menu-routes.js
@@ -1,6 +1,17 @@
 const router = require('express').Router();
+const axios = require('axios').default;
 
 // route /menu/
-router.get('/'); 
+router.get('/', async (req, res) => {
+    const categoryData = await axios.get(`http:/localhost:3001/api/categories`);
+    console.log(categoryData);
+    console.log('Executing...');
+
+    // {categoryData.map((category) => {
+    //     <Category
+    //         name = {category.name}
+    //     />
+    // })}
+}); 
 
 module.exports = router;
