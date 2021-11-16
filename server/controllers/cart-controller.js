f38b7a73ea946c1b8dd57a9843019914e367a15b
Fikcup <rhys.n.wright@gmail.com>
2021-11-16 10:15:55 -0800

    feat : create cart, get user cart, add products to cart, get all products in cart

--- server/controllers/cart-controller.js
+++ server/controllers/cart-controller.js
@@ -1,7 +1,58 @@
-const { Cart } = require('../models');
+const { Cart, CartProducts } = require('../models');
 
 const cartController = {
-
+    newCart(req, res) {
+        Cart.create(req.body)
+            .then((cartData) => {
+                res.json(cartData)
+            })
+            .catch((err) => {
+                console.log(err);
+                res.status(500).json(err);
+            });
+    },
+    getOneCart(req, res) {
+        Cart.findOne({
+            where: {
+                uuid: req.params.cartId
+            }
+        })
+            .then((cartData) => {
+                res.json(cartData)
+            })
+            .catch((err) => {
+                console.log(err);
+                res.status(500).json(err);
+            });
+    },
+    getAllProductsInCart(req, res) {
+        CartProducts.findAll({
+            where: {
+                cartUuid: req.params.cartId
+            }
+        })
+            .then((cartProductData) => {
+                res.json(cartProductData)
+            })
+            .catch((err) => {
+                console.log(err);
+                res.status(500).json(err);
+            });
+    },
+    addProductToCart(req, res) {
+        CartProducts.create({
+            cartUuid: req.params.cartId,
+            productUuid: req.params.productId,
+            quantity: req.body.quantity
+        })
+            .then((cartProductData) => {
+                res.json(cartProductData)
+            })
+            .catch((err) => {
+                console.log(err);
+                res.status(500).json(err);
+            });
+    },
 };
 
 module.exports = cartController;
---
+++ server/routes/api/cart-routes.js
@@ -0,0 +1,21 @@
+const router = require('express').Router();
+const {
+    newCart,
+    getOneCart,
+    getAllProductsInCart,
+    addProductToCart,
+} = require('../../controllers/cart-controller');
+
+// route /api/carts/
+router.route('/').post(newCart);
+
+// route/api/carts/:cartId/
+router.route('/:cartId').get(getOneCart);
+
+// route/api/carts/:cartId/product/
+router.route('/:cartId/product').get(getAllProductsInCart);
+
+// route/api/carts/:cartId/product/:productId/
+router.route('/:cartId/product/:productId').post(addProductToCart);
+
+module.exports = router;
