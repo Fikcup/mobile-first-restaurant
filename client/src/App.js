3f59644675bc067aab6cff865d3c9cc067b42566
Fikcup <rhys.n.wright@gmail.com>
2021-11-17 10:50:57 -0800

    feat : routes for all categories and items

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,54 @@
 import './App.css';
+import React, {useState, useEffect} from 'react';
 import { HashRouter as Router, Routes, Route } from 'react-router-dom'; 
+import axios from 'axios';
 import Footer from './components/Footer';
 import Nav from './components/Nav';
 import Login from './components/Login';
 import Signup from './components/Signup';
 import Menu from './components/Menu';
 import Category from './components/Category';
+import Item from './components/Item';
 
 function App() {
+  const [items, setItems] = useState([]);
+  const [categories, setCategories] = useState([]);
+
+  useEffect(() => {
+    async function getAllMenu() {
+      await axios.get(`/api/categories`)
+        .then(data => {
+          setCategories(data.data);
+        });
+
+      await axios.get(`/api/products`)
+        .then(data => {
+          setItems(data.data);
+        })
+      
+      console.log(items);
+      console.log(categories);
+    }
+
+    getAllMenu();
+  }, [items.uuid, categories.uuid]);
+  // https://dmitripavlutin.com/react-useeffect-infinite-loop/
+
   return (
     <Router>
     <div className="App">
       <Routes>
         <Route exact path="/" />
         <Route exact path="/menu" element={<Menu/>} />
-        <Route exact path="/menu/683f1574-4104-11ec-973a-0242ac130003" element={<Category/>}/>
+        {categories.map((category) => {
+          let link = `/menu/${category.uuid}`;
+          return <Route exact path={link} key={category.uuid} element={<Category/>}/>
+        })}
+        {items.map((item) => {
+          let link = `/menu/item/${item.uuid}`;
+
+          return <Route exact path={link} key={item.uuid} element={<Item/>}/>
+        })}
         <Route exact path="/me" element={<Login/>} />
         <Route exact path="/me/signup" element={<Signup/>} />
         <Route exact path="/cart" />
