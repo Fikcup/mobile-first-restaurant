94281789d35587693920976b1097957ca6dca972
Fikcup <rhys.n.wright@gmail.com>
2021-11-18 17:07:41 -0800

    refactor : token stored in local storage and signed in the backend

--- client/src/components/Login/index.jsx
+++ client/src/components/Login/index.jsx
@@ -1,27 +1,9 @@
 import React from 'react';
 import axios from 'axios';
 import { NavLink, useNavigate } from 'react-router-dom';
-import bcrypt from 'bcryptjs';
-import jwt from 'jsonwebtoken';
 
-const Login = () => {
+const Login = ({ setToken }) => {
     const navigate = useNavigate();
-
-    async function sendToken(authToken) {
-        await axios.get(`/api/users/${authToken.id}`, {
-            headers: {
-                'x-access-token': authToken
-            }
-        })
-            .then(() => {
-                alert('Successful login!');
-                navigate('/menu');
-            })
-            .catch((err) => {
-                console.log(err);
-                alert('Account could not be verified, please try again.');
-            })
-    }
 
     async function verifyUser(event) {
         event.preventDefault();
@@ -33,16 +15,11 @@ const Login = () => {
             email: email,
             password: password
         })
-            .then((userData) => {
-                if (!userData) {
-                    alert('Your email or password is incorrect.');
-                } else {
-                    if (bcrypt.compareSync(password, userData.data.password)) {
-                        let token = jwt.sign({ id: userData.data.uuid}, process.env.REACT_APP_SECRET);
-
-                        sendToken(token);
-                    }
-                }
+            .then((token) => {
+                setToken(token);
+                localStorage.setItem('token', token);
+                alert('Successful login!');
+                navigate('/menu');
             })
             .catch((err) => {
                 console.log(err)
