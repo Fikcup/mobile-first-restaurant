73d23e14c226f462f44ddf03fa140eee4948c31c
Fikcup <rhys.n.wright@gmail.com>
2021-11-20 10:31:34 -0800

    feat : get id from token and use to get user's cart data, get information for each product on cart, render cart product information on page

--- client/src/components/Cart/index.jsx
+++ client/src/components/Cart/index.jsx
@@ -1,15 +1,80 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
+import jwt from 'jsonwebtoken';
 
-const Cart = (props) => {
+const Cart = ({ token }) => {
     // TODO : send token in all cart header requests to verify with backend middleware
+    const [cartProducts, setCartProducts] = useState([]);
+    const [products, setProducts] = useState([]);
+    const [cart, setCart] = useState([]);
+
+    async function mapProducts(product) {
+        if (cartProducts) {
+            let temp = [];
+            for (let i = 0; i < product.length; i++) {
+                await axios.get(`/api/products/${product[i].productUuid}`)
+                    .then((productData) => {
+                        temp.push(productData.data);
+                    })
+                    .catch((err) => {
+                        console.log(err);
+                    });
+            }
+            setProducts(temp);
+        }
+    }
+
+    useEffect(() => {
+        async function getCartProducts(token) {
+            const decoded = jwt.verify(JSON.parse(token).data, process.env.REACT_APP_SECRET);
+            const user = decoded.id;
+
+            await axios.get(`/api/carts/${user}`)
+                .then((cartData) => {
+                    setCart(cartData.data.uuid);
+                })
+                .catch((err) => {
+                    console.log(err);
+                });
+
+            await axios.get(`/api/carts/${cart}/product`)
+                .then((cartData) => {
+                    setCartProducts(cartData.data);
+                })
+                .catch((err) => {
+                    console.log(err);
+                });
+        }
+
+        getCartProducts(token);
+        mapProducts(cartProducts);
+    }, []);
 
     return (
         <div>
             <h1>Cart</h1>
-            
-            {props.children}
 
-            <h2>Order Total: {props.total}</h2>
+            <div>
+                {products.map((product, index) => {
+                    return (
+                        <div key={product.uuid + index}>
+                            <h2>{product.name}</h2>
+                            <img src={product.imgPath} alt={product.name} />
+                            <h3>{product.price}</h3>
+                        </div>
+                    );
+                })}
+
+                {cartProducts.map((cartProduct, index) => {
+                    return (
+                        <div key={cartProduct + index}>
+                            <h3>{cartProduct.quantity}</h3>
+                        </div>
+                    );
+                })}
+            </div>
+
+            <h2>Order Total: {}</h2>
         </div>
     );
 }
