8bebc8ee7dd0bc9e06bccd67ae68fbdfed25d496
Fikcup <rhys.n.wright@gmail.com>
2021-11-20 16:36:17 -0800

    feat : add item from list to cart

--- client/src/components/Category/index.jsx
+++ client/src/components/Category/index.jsx
@@ -1,11 +1,34 @@
 import React, {useState, useEffect} from 'react';
 import { NavLink } from 'react-router-dom';
 import axios from 'axios';
+import jwt from 'jsonwebtoken';
 
-const Category = () => {
+const Category = ({ token }) => {
     const [items, setItems] = useState([]);
+    const [cart, setCart] = useState([])
+
+    async function addToCart(event) {
+        event.preventDefault();
+
+        const item = event.target.parentNode.id;
+        console.log(item);
+
+        await axios.post(`/api/carts/${cart}/product/${item}`)
+            .then((cartData) => {
+                console.log(cartData);
+            })
+    }
 
     useEffect(() => {
+        async function decodeToken() {
+            const decoded = jwt.verify(JSON.parse(token).data, process.env.REACT_APP_SECRET);
+            const user = decoded.id;
+
+            const cartData = await axios.get(`/api/carts/${user}`);
+
+            setCart(cartData.data.uuid);
+        }
+
         async function getAllItemsCategories() {
             let url = window.location.href;
             let categoryId = window.location.href.substr(url.length - 36, url.length);
@@ -13,11 +36,11 @@ const Category = () => {
             await axios.get(`/api/products/category/${categoryId}`)
                 .then(data => {
                     setItems(data.data);
-                    console.log(data.data);
                 });
         }
 
         getAllItemsCategories();
+        decodeToken();
     }, []);
 
     return(
@@ -27,12 +50,12 @@ const Category = () => {
 
                 return (
                     <NavLink to={link} key={item.name + index}>
-                        <div>
+                        <div id={item.uuid}>
                             <img src={item.imgPath} alt={item.name} />                            
                             <h1>{item.name}</h1>
                             <h2>{item.price}</h2>
                             <p>{item.description}</p>
-                            <button>Add</button>
+                            <button onClick={addToCart}>Add</button>
                         </div>
                     </NavLink>
                 );
