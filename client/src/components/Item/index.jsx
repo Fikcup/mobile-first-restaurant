9f3fb55496145e0964481b8a32af8d9fe9c5e1e8
Fikcup <rhys.n.wright@gmail.com>
2021-11-20 16:43:55 -0800

    feat : single item display add to cart functionality

--- client/src/components/Item/index.jsx
+++ client/src/components/Item/index.jsx
@@ -1,11 +1,34 @@
 import React, {useState, useEffect} from 'react';
-import axios from 'axios';
 import { NavLink } from 'react-router-dom';
+import axios from 'axios';
+import jwt from 'jsonwebtoken';
 
-const Item = () => {
+const Item = ({ token }) => {
     const [items, setItems] = useState([]);
+    const [cart, setCart] = useState([]);
+    const [quantity, setQuantity] = useState(0);
+
+    async function addToCart(event) {
+        event.preventDefault();
+
+        const item = event.target.id;
+
+        await axios.post(`/api/carts/${cart}/product/${item}`)
+            .then((cartData) => {
+                console.log(cartData);
+            })
+    }
 
     useEffect(() => {
+        async function decodeToken() {
+            const decoded = jwt.verify(JSON.parse(token).data, process.env.REACT_APP_SECRET);
+            const user = decoded.id;
+
+            const cartData = await axios.get(`/api/carts/${user}`);
+
+            setCart(cartData.data.uuid);
+        }
+
         async function getAllItemsCategories() {
             let url = window.location.href;
             let itemId = window.location.href.substr(url.length - 36, url.length);
@@ -18,6 +41,7 @@ const Item = () => {
         }
 
         getAllItemsCategories();
+        decodeToken();
     }, []);
 
     // TODO: add onChange event for quantity
@@ -35,7 +59,7 @@ const Item = () => {
             <h1>{items.name}</h1>
             <p>{items.description}</p>
             <NavLink to="/cart">
-                <button>Add</button>
+                <button id={items.uuid} onClick={addToCart}>Add</button>
             </NavLink>
         </div>
     );
