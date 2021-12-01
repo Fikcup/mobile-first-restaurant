4847d7da4a7fa4666e0f0ad68780a811aad921e5
Fikcup <rhys.n.wright@gmail.com>
2021-11-30 20:14:31 -0800

    feat : added donut image links

--- server/migrations/product-create-product.js
+++ server/migrations/product-create-product.js
@@ -9,7 +9,7 @@ module.exports = {
                 name: 'Apple', 
                 description: 'Crisp apple with hints of sweet caramel.',
                 category_uuid: '5bcead9a-4104-11ec-973a-0242ac130003',
-                imgPath: '',
+                image: 'https://drive.google.com/uc?export=view&id=1scMplRkflGHtCceP78F16hpdYjpgwBAg',
                 price: 1.50
             },
             { 
@@ -17,7 +17,7 @@ module.exports = {
                 name: 'Bluenicorn', 
                 description: 'Blue raspberry drizzled with electric lemon.',
                 category_uuid: '5bcead9a-4104-11ec-973a-0242ac130003',
-                imgPath: '',
+                image: 'https://drive.google.com/uc?export=view&id=1mwgJL0Z5eowJXB73TiMgsPJ_4bI04ZmC',
                 price: 1.50
             },
             { 
@@ -25,7 +25,7 @@ module.exports = {
                 name: 'Choco Glaze', 
                 description: 'Glazed donut with a semi-sweet chocolate drizzle.',
                 category_uuid: '5bcead9a-4104-11ec-973a-0242ac130003',
-                imgPath: '',
+                image: 'https://drive.google.com/uc?export=view&id=1XF5rqvG0oxBKhSPmXVVvFckVnSo9EGXV',
                 price: 1.50
             },
             { 
@@ -33,7 +33,7 @@ module.exports = {
                 name: 'Chocolate Sprinkle', 
                 description: 'Chocolate covered donut with rainbow sprinkles.',
                 category_uuid: '5bcead9a-4104-11ec-973a-0242ac130003',
-                imgPath: '',
+                image: 'https://drive.google.com/uc?export=view&id=1nBC1iuLWaNL5vOwJbWwYYQnc8VUP18VJ',
                 price: 1.50 
             },
             { 
@@ -41,15 +41,15 @@ module.exports = {
                 name: 'Choco Malt', 
                 description: 'Chocolate with crunchy mini malt balls sprinkled over the top.',
                 category_uuid: '5bcead9a-4104-11ec-973a-0242ac130003',
-                imgPath: '',
+                image: 'https://drive.google.com/uc?export=view&id=124ZOD8-1xlsvE_Ij6S0qtejE5I2pvbCR',
                 price: 1.50
             },
             { 
                 uuid: UUIDV1(), 
                 name: 'Coconut', 
-                description: 'Vanilla blue glaze with shredded coconut and flavored sprinkles.',
+                description: 'Vanilla blue glaze with shredded coconut and coconut flakes.',
                 category_uuid: '5bcead9a-4104-11ec-973a-0242ac130003',
-                imgPath: '',
+                image: 'https://drive.google.com/uc?export=view&id=1IN-WzNNtATK8fa7V6IJrwDa_rG_um-b2',
                 price: 1.50
             },
             { 
@@ -57,7 +57,7 @@ module.exports = {
                 name: 'Lemon Zest', 
                 description: 'Zesty lemon glaze topped with vanilla drizzle.',
                 category_uuid: '5bcead9a-4104-11ec-973a-0242ac130003',
-                imgPath: '',
+                image: 'https://drive.google.com/uc?export=view&id=1Bs0H5MXNynFDEl3PG8JO4jvk9A3BKrRD',
                 price: 1.50
             },
             { 
@@ -65,7 +65,7 @@ module.exports = {
                 name: 'Orange Berry', 
                 description: 'Citrusy orange donut with berry slices on top.',
                 category_uuid: '5bcead9a-4104-11ec-973a-0242ac130003',
-                imgPath: '',
+                image: 'https://drive.google.com/uc?export=view&id=13I1MlTNv0N6Redhm1wTsji4PLwZy25l-',
                 price: 1.50
             },
             { 
@@ -73,7 +73,7 @@ module.exports = {
                 name: 'Orange Confetti', 
                 description: 'Citrusy orange donut with flavored sprinkles.',
                 category_uuid: '5bcead9a-4104-11ec-973a-0242ac130003',
-                imgPath: '',
+                image: 'https://drive.google.com/uc?export=view&id=1ZRZyo4n1tFgka6MNp_MWfxgL_SUQMDuW',
                 price: 1.50
             },
             { 
@@ -81,7 +81,7 @@ module.exports = {
                 name: 'Orange Tang', 
                 description: 'Orange all around.',
                 category_uuid: '5bcead9a-4104-11ec-973a-0242ac130003',
-                imgPath: '',
+                image: 'https://drive.google.com/uc?export=view&id=1epUl6Nq5p_-qzZI6Um2RoOMpxSqrsr1O',
                 price: 1.50
             },
             { 
@@ -89,7 +89,7 @@ module.exports = {
                 name: 'Pink Paradise', 
                 description: 'Strawberry glaze with strawberry cream sprinkles.',
                 category_uuid: '5bcead9a-4104-11ec-973a-0242ac130003',
-                imgPath: '',
+                image: 'https://drive.google.com/uc?export=view&id=1ZkTLHzTPZhkJ_Hu74GEbLk4ZLIkNr5Lb',
                 price: 1.50
             },
             { 
@@ -97,7 +97,7 @@ module.exports = {
                 name: 'Purple Passion', 
                 description: 'Acai flavored glaze with raspberry drizzle.',
                 category_uuid: '5bcead9a-4104-11ec-973a-0242ac130003',
-                imgPath: '',
+                image: 'https://drive.google.com/uc?export=view&id=1olXvsQIHqINdKy81_iOWo7TdfPv6XWzb',
                 price: 1.50
             },
             { 
@@ -105,7 +105,7 @@ module.exports = {
                 name: 'Sparkle Sprinkle', 
                 description: 'Raspberry drizzle coated in shreds of vanilla.',
                 category_uuid: '5bcead9a-4104-11ec-973a-0242ac130003',
-                imgPath: '',
+                image: 'https://drive.google.com/uc?export=view&id=1zkUHO_0FJkJyoAL_qChq1qUMYqUpgDmz',
                 price: 1.50
             },
             { 
@@ -113,7 +113,7 @@ module.exports = {
                 name: 'Strawberry', 
                 description: 'Strawberry jam infused glaze over a plain donut.',
                 category_uuid: '5bcead9a-4104-11ec-973a-0242ac130003',
-                imgPath: '',
+                image: 'https://drive.google.com/uc?export=view&id=1n5A_PtjdLV_R7XfOWqjiafZBePPbI4g-',
                 price: 1.50
             },
             { 
@@ -121,15 +121,7 @@ module.exports = {
                 name: 'Vanilla', 
                 description: 'Vanilla glaze with sweet sprinkles.',
                 category_uuid: '5bcead9a-4104-11ec-973a-0242ac130003',
-                imgPath: '',
-                price: 1.50
-            },
-            { 
-                uuid: UUIDV1(), 
-                name: '', 
-                description: '',
-                category_uuid: '',
-                imgPath: '',
+                image: 'https://drive.google.com/uc?export=view&id=1wVD8sqPbpYTdo86PHLjjnKu_RNyOWoJs',
                 price: 1.50
             }
         ]);
