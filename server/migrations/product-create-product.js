019640ddd3ed02d7d1b7bf8b67609f2f992324fb
Fikcup <rhys.n.wright@gmail.com>
2021-11-30 18:02:52 -0800

    feat : donut seeds

--- server/migrations/product-create-product.js
+++ server/migrations/product-create-product.js
@@ -6,102 +6,132 @@ module.exports = {
         await queryInterface.bulkInsert('Product', [
             { 
                 uuid: UUIDV1(), 
-                name: 'Chocolate Chip Pancakes', 
-                description: 'Decadent and rich chocolate chips in our signature pancake batter.',
+                name: 'Apple', 
+                description: 'Crisp apple with hints of sweet caramel.',
                 category_uuid: '5bcead9a-4104-11ec-973a-0242ac130003',
-                price: 8.50
+                imgPath: '',
+                price: 1.50
             },
             { 
                 uuid: UUIDV1(), 
-                name: 'Buttermilk Pancakes', 
-                description: 'If it ain\'t broke don\'t fix it! Signature pancake batter cooked with butter and love.',
+                name: 'Bluenicorn', 
+                description: 'Blue raspberry drizzled with electric lemon.',
                 category_uuid: '5bcead9a-4104-11ec-973a-0242ac130003',
-                price: 7.00 
+                imgPath: '',
+                price: 1.50
             },
             { 
                 uuid: UUIDV1(), 
-                name: 'Blueberry Pancakes', 
-                description: 'Farm fresh blueberries in our cooked to perfection, signature pancake batter.',
+                name: 'Choco Glaze', 
+                description: 'Glazed donut with a semi-sweet chocolate drizzle.',
                 category_uuid: '5bcead9a-4104-11ec-973a-0242ac130003',
-                price: 9.00 
+                imgPath: '',
+                price: 1.50
             },
             { 
                 uuid: UUIDV1(), 
-                name: 'Ham and Cheese Omelet', 
-                description: 'Three eggs, honey glazed chunks of ham in cheesy cheddar.',
-                category_uuid: '683f1574-4104-11ec-973a-0242ac130003',
-                price: 12.00
+                name: 'Chocolate Sprinkle', 
+                description: 'Chocolate covered donut with rainbow sprinkles.',
+                category_uuid: '5bcead9a-4104-11ec-973a-0242ac130003',
+                imgPath: '',
+                price: 1.50 
             },
             { 
                 uuid: UUIDV1(), 
-                name: 'Denver Omelet', 
-                description: 'Three eggs, green bell peppers, onions, and white cheddar.',
-                category_uuid: '683f1574-4104-11ec-973a-0242ac130003',
-                price: 12.00 
+                name: 'Choco Malt', 
+                description: 'Chocolate with crunchy mini malt balls sprinkled over the top.',
+                category_uuid: '5bcead9a-4104-11ec-973a-0242ac130003',
+                imgPath: '',
+                price: 1.50
             },
             { 
                 uuid: UUIDV1(), 
-                name: 'Chili Cheese Omelet', 
-                description: 'Three eggs, chili, and American cheese.',
-                category_uuid: '683f1574-4104-11ec-973a-0242ac130003',
-                price: 12.00 
+                name: 'Coconut', 
+                description: 'Vanilla blue glaze with shredded coconut and flavored sprinkles.',
+                category_uuid: '5bcead9a-4104-11ec-973a-0242ac130003',
+                imgPath: '',
+                price: 1.50
             },
             { 
                 uuid: UUIDV1(), 
-                name: 'Bacon, Egg, Potato Skillet', 
-                description: 'Two scrambled eggs, chunks of bacon, and country potato.',
-                category_uuid: '76a2d90c-4104-11ec-973a-0242ac130003',
-                price: 11.50
+                name: 'Lemon Zest', 
+                description: 'Zesty lemon glaze topped with vanilla drizzle.',
+                category_uuid: '5bcead9a-4104-11ec-973a-0242ac130003',
+                imgPath: '',
+                price: 1.50
             },
             { 
                 uuid: UUIDV1(), 
-                name: 'Loaded Skillet', 
-                description: 'Two scrambled eggs, cheddar cheese, chunks of ham, bacon, and country potato',
-                category_uuid: '76a2d90c-4104-11ec-973a-0242ac130003',
-                price: 14.00
+                name: 'Orange Berry', 
+                description: 'Citrusy orange donut with berry slices on top.',
+                category_uuid: '5bcead9a-4104-11ec-973a-0242ac130003',
+                imgPath: '',
+                price: 1.50
             },
             { 
                 uuid: UUIDV1(), 
-                name: 'BLT', 
-                description: 'A classic BLT sandwhich.',
-                category_uuid: '8fef7b86-4104-11ec-973a-0242ac130003',
-                price: 10.00
+                name: 'Orange Confetti', 
+                description: 'Citrusy orange donut with flavored sprinkles.',
+                category_uuid: '5bcead9a-4104-11ec-973a-0242ac130003',
+                imgPath: '',
+                price: 1.50
             },
             { 
                 uuid: UUIDV1(), 
-                name: 'Egg and Cheese Breakfast Sandwhich', 
-                description: 'Two scrambled eggs and cheddar cheese',
-                category_uuid: '8fef7b86-4104-11ec-973a-0242ac130003',
-                price: 9.00
+                name: 'Orange Tang', 
+                description: 'Orange all around.',
+                category_uuid: '5bcead9a-4104-11ec-973a-0242ac130003',
+                imgPath: '',
+                price: 1.50
             },
             { 
                 uuid: UUIDV1(), 
-                name: 'Bacon, Egg, and Cheese Breakfast Sandwhich', 
-                description: 'Two scrambled eggs topped with bacon strips and cheddar cheese.',
-                category_uuid: '8fef7b86-4104-11ec-973a-0242ac130003',
-                price: 10.00
+                name: 'Pink Paradise', 
+                description: 'Strawberry glaze with strawberry cream sprinkles.',
+                category_uuid: '5bcead9a-4104-11ec-973a-0242ac130003',
+                imgPath: '',
+                price: 1.50
             },
             { 
                 uuid: UUIDV1(), 
-                name: 'Chocolate Chip Waffle', 
-                description: 'Semi-sweet chocolate chips in our signature waffle batter.',
-                category_uuid: '939ec372-4104-11ec-973a-0242ac130003',
-                price: 8.50 
+                name: 'Purple Passion', 
+                description: 'Acai flavored glaze with raspberry drizzle.',
+                category_uuid: '5bcead9a-4104-11ec-973a-0242ac130003',
+                imgPath: '',
+                price: 1.50
             },
             { 
                 uuid: UUIDV1(), 
-                name: 'Buttermilk Waffle', 
-                description: 'Hearty and thick waffles made fresh to order.',
-                category_uuid: '939ec372-4104-11ec-973a-0242ac130003',
-                price: 7.00 
+                name: 'Sparkle Sprinkle', 
+                description: 'Raspberry drizzle coated in shreds of vanilla.',
+                category_uuid: '5bcead9a-4104-11ec-973a-0242ac130003',
+                imgPath: '',
+                price: 1.50
             },
             { 
                 uuid: UUIDV1(), 
-                name: 'Blueberry Waffle', 
-                description: 'Farm fresh blueberries in our cooked to perfection, signature waffle batter.',
-                category_uuid: '939ec372-4104-11ec-973a-0242ac130003',
-                price: 9.00 
+                name: 'Strawberry', 
+                description: 'Strawberry jam infused glaze over a plain donut.',
+                category_uuid: '5bcead9a-4104-11ec-973a-0242ac130003',
+                imgPath: '',
+                price: 1.50
             },
+            { 
+                uuid: UUIDV1(), 
+                name: 'Vanilla', 
+                description: 'Vanilla glaze with sweet sprinkles.',
+                category_uuid: '5bcead9a-4104-11ec-973a-0242ac130003',
+                imgPath: '',
+                price: 1.50
+            },
+            { 
+                uuid: UUIDV1(), 
+                name: '', 
+                description: '',
+                category_uuid: '',
+                imgPath: '',
+                price: 1.50
+            }
         ]);
     },
 
