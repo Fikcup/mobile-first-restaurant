54da35694e920e0b17d1393b90b82d18d26e758d
Fikcup <rhys.n.wright@gmail.com>
2021-12-02 18:10:15 -0800

    feat : added logout and delete account functionality

--- client/src/components/Account/index.jsx
+++ client/src/components/Account/index.jsx
@@ -1,9 +1,42 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import jwt from 'jsonwebtoken';
 
 const Account = ({ token }) => {
+    const [user, setUser] = useState();
+
+    async function deleteAccount() {
+        const verify = window.confirm('Are you sure you want to delete your account?');
+        
+        if (verify) {
+            await axios.delete(`/api/users/${user.uuid}`);
+            localStorage.removeItem('token');
+            window.location.reload();
+        }
+    }
+
+    const logout = () => {
+        localStorage.removeItem('token');
+        window.location.reload();
+    }
+
+    useEffect(() => {
+        (async () => {
+            const decoded = jwt.verify(token, process.env.REACT_APP_SECRET);
+            const userId = decoded.id;
+
+            const account = await axios.get(`/api/users/${userId}`);
+
+            setUser(account.data);
+        })();
+    }, [])
+
     return(
         <div>
             <h1>Account</h1>
+
+            <button onClick={logout}>Logout</button>
+            <button onClick={deleteAccount}>Delete account</button>    
         </div>
     );
 }
