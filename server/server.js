69cb05e0c7720ce8ddcda158fb6330beb36091f3
Fikcup <rhys.n.wright@gmail.com>
2021-12-07 17:05:49 -0800

    refactor : removed unused dependency

--- server/server.js
+++ server/server.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const cors = require('cors');
-const bodyParser = require('body-parser');
 const logger = require('morgan');
 
 const sequelize = require('./config/connection');
